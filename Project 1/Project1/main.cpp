c162475169869bbac17ccb977e44bf41144a67d0
evenmn <evenmn@fys.uio.no>
2018-01-28 13:42:02 +0100

    Added wf for N particles in 3 dimensions

--- Project 1/Project1/main.cpp
+++ Project 1/Project1/main.cpp
@@ -9,6 +9,37 @@ double random_position(double steplength){
     return ((double)rand() / (double)RAND_MAX)*steplength;
 }
 
+double u(double rij) {
+    /*u takes the position of two particles as arguments
+    and returns the attraction force between them */
+
+    return 1/rij;
+}
+
+double wf(double array[][3], int N, double alpha, double beta) {
+    /* Returns the wavefunction based on an array of
+    positions preferably in three dimensions */
+
+    double sumsqrt = 0;         // x1^2 + y1^2 + B*z1^2 + ... + B*zn^2
+    double sumu = 0;            // sum(u(rij))
+
+    for(int i=0; i<N; i++) {
+        sumsqrt += array[i][0]*array[i][0] + \
+                   array[i][1]*array[i][1] + \
+                   beta*array[i][2]*array[i][2];
+
+        for(int j=N; j>i; j--) {
+            double rij = sqrt((array[i][0]-array[j][0])*(array[i][0]-array[j][0]) + \
+                              (array[i][1]-array[j][1])*(array[i][1]-array[j][1]) + \
+                              (array[i][2]-array[j][2])*(array[i][2]-array[j][2]));
+            sumu += u(rij);
+
+        }
+    }
+    //cout << sumu << " " << sumsqrt << endl;
+    return exp(-alpha*sumsqrt)*exp(sumu);
+}
+
 int main()
 {
     //loop over several alphas
@@ -30,6 +61,16 @@ int main()
     //
     double psi_ratio;
     double r;
+
+    //Wavefunction for N particles in 3 dim
+    int N = 10;
+    double array [N][3];
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<3; j++) {
+            array[i][j] = random_position(1);
+        }
+    }
+    cout << "Probability: " << wf(array, N, 1, 1)*wf(array, N, alpha, beta) << endl;
 
     WaveFunction Psi;
     Psi.setTrialWF(1,1);
