48f4df0a6ecfff166607456c2ebf8a902ab55eba
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2018-02-07 12:39:30 +0100

    BF_met files now includes the brute force Metropolis algorithm, runs correctly

--- Project 1/Project1/BF_met.cpp
+++ Project 1/Project1/BF_met.cpp
@@ -1,10 +1,93 @@
 #include <BF_met.h>
 #include<iostream>
 #include<cmath>
+#include<wavefunction.h>
+#include <string.h>
+#include<vector>
 
 using namespace std;
 
-void BF_calc(int N, int dim, int M, double a, double steplength, double omega_HO, double omega_Z, int HO, double alpha, double beta)
+double random_position_1(double steplength){
+    return ((double)rand() / (double)RAND_MAX)*steplength;
+}
+
+void BF_calc(int N, int dim, int M, double a, double steplength, double omega_HO, double omega_z, int HO, double alpha[], int length_alpha_1, double beta)
 {
-    cout << "Hey" << endl;
+    cout << length_alpha_1 << endl;
+    cout << alpha[0] << " " << alpha[1] << " "<< alpha[2] << endl;
+
+    for(int k=0; k<length_alpha_1; k++){
+        //averages and energies
+        double E_tot_1 = 0;           //sum of energies of all states
+        double E_tot_sqrd_1 = 0;      //sum of energies of all states squared
+        double E_1 = 0;               //energy after change in position
+        double E_prev_1 = 0;          //energy before change in position
+        double delta_EL_1;            //change in energy
+
+        double psi_ratio_1;           //ratio of new and old wave function
+        double r_1;                   //random number
+        int N_rand_1;                 //randomly chosen N
+        int dim_rand_1;               //randomly chosen dimension
+        double pos_mat_new_1[N][3];   //new position with random position
+
+        int num_or_an_1 = 0;
+
+        //Initialize position matrix for N particles in dim dimentions
+        double pos_mat_1[N][3];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<dim; j++) {
+                pos_mat_1[i][j] = random_position_1(1);
+            }
+            for(int k=dim; k<3; k++){
+                pos_mat_1[i][k] = 0;
+            }
+        }
+
+        //Initialize wave function
+        WaveFunction Psi_1;
+        Psi_1.setTrialWF(dim, N, a, num_or_an_1, HO);
+
+        //Add initial energies to averages
+        E_1 = Psi_1.E_L(pos_mat_1, alpha[k], beta, omega_HO, omega_z);
+        E_tot_1 += E_1;
+        E_tot_sqrd_1 += E_1*E_1;
+
+        //Start Monte Carlo iterations
+        for(int i=0;i<M;i++){
+            //Draw random position, for one particle and one dimention
+            N_rand_1 = rand()%N;
+            dim_rand_1 = rand()%dim;
+
+            //Set new meatrix equal old one
+            memcpy(pos_mat_new_1, pos_mat_1, sizeof(pos_mat_new_1));
+
+            //Proposed new position
+            pos_mat_new_1[N_rand_1][dim_rand_1] = pos_mat_1[N_rand_1][dim_rand_1] + (random_position_1(1)-0.5)*steplength;
+
+            //Metropolis algorithm
+            psi_ratio_1 = Psi_1.Psi_value_sqrd(pos_mat_new_1, alpha[k], beta)/(Psi_1.Psi_value_sqrd(pos_mat_1, alpha[k], beta));
+            E_prev_1 = Psi_1.E_L(pos_mat_1, alpha[k], beta, omega_HO, omega_z);
+
+            //cout << Psi.E_L_num(pos_mat, alpha[k], beta, 0.001) << endl;
+
+            r_1 = ((double)rand() / (double)RAND_MAX);
+
+            if(psi_ratio_1 >= r_1){
+                //accept and update pos_mat
+                memcpy(pos_mat_1, pos_mat_new_1, sizeof(pos_mat_1)); //maybe more time efficient to only update the one changed position?
+            }
+
+            E_1 = Psi_1.E_L(pos_mat_1, alpha[k], beta, omega_HO, omega_z);
+            delta_EL_1 = E_1 - E_prev_1;
+            E_tot_1 += E_1;
+            E_tot_sqrd_1 += E_1*E_1;
+        }
+
+        //Calculate <E_l> and <E_L**2>
+        double E_L_avg_1 = E_tot_1/M;
+        double E_L_avg_sqrd_1 = E_tot_sqrd_1/M;
+
+        cout << "E_L_avg: " << E_L_avg_1 << endl;
+        cout << "E_L_avg_tot: " << E_L_avg_sqrd_1 << "\n" << endl;
+    }
 }
--- Project 1/Project1/main.cpp
+++ Project 1/Project1/main.cpp
@@ -4,6 +4,7 @@
 #include <wavefunction.h>
 #include <string.h>
 #include <vector>
+#include <BF_met.h>
 using namespace std;
 
 double random_position(double steplength){
@@ -23,6 +24,8 @@ int main()
     int num_or_an = 0;          //if calculation is to be based on analytical(0) or numerical(1) E_L
     int HO = 0;                 //spherical (0) or elliptical (1) harmonic oscillator
     double a = 0;               //distance parameter
+    double alpha[] = {0.25, 0.5, 1.0, 1.5};           //variational parameter
+    int length_alpha_1 = sizeof(alpha)/sizeof(*alpha);
 
     /*
     vector<vector<double>> posmat;
@@ -32,86 +35,9 @@ int main()
 
     for(auto* i : posmat)
         i.resize(dim);
-
    */
 
-    //loop over several alphas
-    double alpha[] = {0.1, 0.25, 0.5, 0.75, 1.0};           //variational parameter
-    int length_alpha = sizeof(alpha)/sizeof(*alpha);
+    BF_calc(N, dim, M, a, steplength, omega_HO, omega_z, HO, alpha, length_alpha_1, beta);
 
-    //Everything below here could be put in own script
-    for(int k=0; k<length_alpha; k++){
-        //averages and energies
-        double E_tot = 0;           //sum of energies of all states
-        double E_tot_sqrd = 0;      //sum of energies of all states squared
-        double E = 0;               //energy after change in position
-        double E_prev = 0;          //energy before change in position
-        double delta_EL;            //change in energy
-
-        double psi_ratio;           //ratio of new and old wave function
-        double r;                   //random number
-        int N_rand;                 //randomly chosen N
-        int dim_rand;               //randomly chosen dimension
-        double pos_mat_new[N][3];   //new position with random position
-
-        //Initialize position matrix for N particles in dim dimentions
-        double pos_mat[N][3];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<dim; j++) {
-                pos_mat[i][j] = random_position(1);
-            }
-            for(int k=dim; k<3; k++){
-                pos_mat[i][k] = 0;
-            }
-        }
-
-
-        //Initialize wave function
-        WaveFunction Psi;
-        Psi.setTrialWF(dim, N, a, num_or_an, HO);
-
-        //Add initial energies to averages
-        E = Psi.E_L(pos_mat, alpha[k], beta, omega_HO, omega_z);
-        E_tot += E;
-        E_tot_sqrd += E*E;
-
-        //Start Monte Carlo iterations
-        for(int i=0;i<M;i++){
-            //Draw random position, for one particle and one dimention
-            N_rand = rand()%N;
-            dim_rand = rand()%dim;
-
-            //Set new meatrix equal old one
-            memcpy(pos_mat_new, pos_mat, sizeof(pos_mat_new));
-
-            //Proposed new position
-            pos_mat_new[N_rand][dim_rand] = pos_mat[N_rand][dim_rand] + (random_position(1)-0.5)*steplength;
-
-            //Metropolis algorithm
-            psi_ratio = Psi.Psi_value_sqrd(pos_mat_new, alpha[k], beta)/(Psi.Psi_value_sqrd(pos_mat, alpha[k], beta));
-            E_prev = Psi.E_L(pos_mat, alpha[k], beta, omega_HO, omega_z);
-
-            //cout << Psi.E_L_num(pos_mat, alpha[k], beta, 0.001) << endl;
-
-            r = ((double)rand() / (double)RAND_MAX);
-
-            if(psi_ratio >= r){
-                //accept and update pos_mat
-                memcpy(pos_mat, pos_mat_new, sizeof(pos_mat)); //maybe more time efficient to only update the one changed position?
-            }
-
-            E = Psi.E_L(pos_mat, alpha[k], beta, omega_HO, omega_z);
-            delta_EL = E - E_prev;
-            E_tot += E;
-            E_tot_sqrd += E*E;
-        }
-
-        //Calculate <E_l> and <E_L**2>
-        double E_L_avg = E_tot/M;
-        double E_L_avg_sqrd = E_tot_sqrd/M;
-
-        cout << "E_L_avg: " << E_L_avg << endl;
-        cout << "E_L_avg_tot: " << E_L_avg_sqrd << "\n" << endl;
-    }
     return 0;
 }
