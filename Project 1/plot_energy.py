5d5bb12754adc0bb9aa881673e2d2795809b4aa7
evenmn <evenmn@fys.uio.no>
2018-02-23 17:58:26 +0100

    Tried to add some folders to maintain an acceptable structure of the repository

--- Project 1/plot_energy.py
+++ Project 1/plot_energy.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 #Analytical values
 def E_L(alpha):
     return alpha
 
 # Numerical values
-data = np.loadtxt("local_energy.dat")
+dirpath = os.getcwd()
+print(dirpath)
+data = np.loadtxt("%s/data/local_energy.dat" % dirpath)
+print(data)
 
 sns.set()
 plt.plot(data[:,0], data[:,1], label="Numerical")
@@ -15,7 +19,7 @@ plt.title("Local energy")
 plt.xlabel(r"$\alpha$")
 plt.ylabel(r"$E_L(\alpha)$")
 plt.legend(loc="best")
-plt.savefig("energy.png")
+plt.savefig("%s/images/energy.png" % dirpath)
 plt.show()
 
 plt.plot(data[:,0], data[:,2], label="Numerical")
@@ -23,5 +27,5 @@ plt.title("Variance")
 plt.xlabel(r"$\alpha$")
 plt.ylabel(r"$\sigma$")
 plt.legend(loc="best")
-plt.savefig("variance.png")
+plt.savefig("%s/images/variance.png" % dirpath)
 plt.show()
