14f8795b71815d346d85010e8337f67ca56dd6ea
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2018-03-16 08:34:04 +0100

    Merge

--- Project 1/Project1/main.cpp
+++ Project 1/Project1/main.cpp
@@ -7,33 +7,25 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     //variables chosen by user
-<<<<<<< HEAD
-    int    M          = 100000; //number of MC cycles
+    int    M          = 1000000; //number of MC cycles
     int    N          = 10;      //number of particles
-=======
-    int    M          = 10000000; //number of MC cycles
-    int    N          = 1;      //number of particles
->>>>>>> 4ebdd2cddf0edc58e515c34794a470e1796080d8
     int    dim        = 3;       //number of dimensions concidered
     double beta       = 2.82843;       //weight parameter along z-axis
     double steplength = 1.0;     //steplength when changing position
     double h          = 0.01;    //Step length for numerical double differentiation
     double timestep   = 1;     //Timestep, to be used in Metropolis-Hastings
-    double a          = 0;     //distance parameter
+    double a          = 0.0043;     //distance parameter
 
     //Choices of simulation types
-    bool   HO         = true;    //spherical (true) or elliptical (false) harmonic oscillator
+    bool   HO         = false;    //spherical (true) or elliptical (false) harmonic oscillator
     int    BF_H       = 0;       //brute force (0) or hastings(1) metropolis algorithm
     int    one_body   = 0;       //calculate one body density (1) or not calculate (0)
     int    num_or_an  = 0;       //if calculation is to be based on analytical(0) or numerical(1) E_L
 
 
-<<<<<<< HEAD
-=======
-    double alpha[]    = { 0.3, 0.35, 0.4, 0.5};           //variational parameter
+    double alpha[]    = {0.2};           //variational parameter
     int    len_alpha  = sizeof(alpha)/sizeof(*alpha);    //length of alpha
 
->>>>>>> 4ebdd2cddf0edc58e515c34794a470e1796080d8
     cout << "Running with the following paramteres:" << endl;
     cout << "Number of particles: " << N << endl;
     cout << "Number of dimensions: " << dim << endl;
@@ -43,13 +35,14 @@ int main(int argc, char* argv[])
     cout << "Brute force(0) or Hastings(1) Metropolis algo:" << BF_H << endl;
     cout << "One body calculations active (1): " << one_body << "\n" << endl;
 
-    double alpha_opt = GradientDecent(N, dim, M, a, steplength, HO, beta, h, num_or_an, BF_H, timestep);
-    cout << "Optimal alpha: " << alpha_opt << "\n" << endl;
+    //double alpha_opt = GradientDecent(N, dim, M, a, steplength, HO, beta, h, num_or_an, BF_H, timestep);
+    //cout << "Optimal alpha: " << alpha_opt << "\n" << endl;
 
-    int M_met = 1000000;
-    double alpha[]    = {alpha_opt};           //variational parameter
-    int    len_alpha  = sizeof(alpha)/sizeof(*alpha);    //length of alpha
-    Met_algo(N, dim, M_met, a, steplength, HO, alpha, len_alpha, beta, h, num_or_an, BF_H, timestep, one_body);
+    //int M_met = 1000000;
+    //double alpha[]    = {alpha_opt};           //variational parameter
+    //int    len_alpha  = sizeof(alpha)/sizeof(*alpha);    //length of alpha
+    Met_algo(N, dim, M, a, steplength, HO, alpha, len_alpha, beta, h, num_or_an, BF_H, timestep, one_body);
+    //GradientDecent(N, dim, M, a, steplength, HO, beta, h, num_or_an, BF_H, timestep);
 
     return 0;
 }
