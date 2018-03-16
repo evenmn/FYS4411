10806c0378a5331661b22285ca49dcaadca515fe
Even <evenmn@fys.uio.no>
2018-03-16 17:38:47 +0100

    Even more corrections

--- Project 1/Project1/main.cpp
+++ Project 1/Project1/main.cpp
@@ -10,20 +10,20 @@ int main(int argc, char* argv[])
     int    M          = 1000000; //number of MC cycles
     int    N          = 100;      //number of particles
     int    dim        = 3;       //number of dimensions concidered
-    double beta       = 2.82843;       //weight parameter along z-axis
+    double beta       = 1;       //weight parameter along z-axis
     double steplength = 1.0;     //steplength when changing position
     double h          = 0.01;    //Step length for numerical double differentiation
     double timestep   = 1;     //Timestep, to be used in Metropolis-Hastings
-    double a          = 0.0043;     //distance parameter
+    double a          = 0;     //distance parameter
 
     //Choices of simulation types
-    bool   HO         = false;    //spherical (true) or elliptical (false) harmonic oscillator
+    bool   HO         = true;    //spherical (true) or elliptical (false) harmonic oscillator
     int    BF_H       = 0;       //brute force (0) or hastings(1) metropolis algorithm
     int    one_body   = 0;       //calculate one body density (1) or not calculate (0)
-    int    num_or_an  = 0;       //if calculation is to be based on analytical(0) or numerical(1) E_L
+    int    num_or_an  = 1;       //if calculation is to be based on analytical(0) or numerical(1) E_L
 
 
-    double alpha[]    = {0.2, 0.3, 0.35, 0.4, 0.5};           //variational parameter
+    double alpha[]    = {0.5};           //variational parameter
     int    len_alpha  = sizeof(alpha)/sizeof(*alpha);    //length of alpha
 
     cout << "Running with the following paramteres:" << endl;
