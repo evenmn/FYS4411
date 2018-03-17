b5d45ec112957829fd1ed35730b219ff7ef5f6c7
evenmn <evenmn@fys.uio.no>
2018-03-17 12:35:21 +0100

    Added wave function results and updated introduction

--- Project 1/Project1/main.cpp
+++ Project 1/Project1/main.cpp
@@ -8,19 +8,19 @@ int main(int argc, char* argv[])
 {
     //variables chosen by user
     int    M          = 1000000; //number of MC cycles
-    int    N          = 100;      //number of particles
+    int    N          = 30;      //number of particles
     int    dim        = 3;       //number of dimensions concidered
-    double beta       = 1;       //weight parameter along z-axis
+    double beta       = 2.82843;       //weight parameter along z-axis
     double steplength = 1.0;     //steplength when changing position
     double h          = 0.01;    //Step length for numerical double differentiation
     double timestep   = 1;     //Timestep, to be used in Metropolis-Hastings
-    double a          = 0;     //distance parameter
+    double a          = 0.0043;     //distance parameter
 
     //Choices of simulation types
-    bool   HO         = true;    //spherical (true) or elliptical (false) harmonic oscillator
+    bool   HO         = false;    //spherical (true) or elliptical (false) harmonic oscillator
     int    BF_H       = 0;       //brute force (0) or hastings(1) metropolis algorithm
     int    one_body   = 0;       //calculate one body density (1) or not calculate (0)
-    int    num_or_an  = 1;       //if calculation is to be based on analytical(0) or numerical(1) E_L
+    int    num_or_an  = 0;       //if calculation is to be based on analytical(0) or numerical(1) E_L
 
 
     double alpha[]    = {0.5};           //variational parameter
@@ -41,8 +41,8 @@ int main(int argc, char* argv[])
     //int M_met = 1000000;
     //double alpha[]    = {alpha_opt};           //variational parameter
     //int    len_alpha  = sizeof(alpha)/sizeof(*alpha);    //length of alpha
-    Met_algo(N, dim, M, a, steplength, HO, alpha, len_alpha, beta, h, num_or_an, BF_H, timestep, one_body);
-    //GradientDecent(N, dim, M, a, steplength, HO, beta, h, num_or_an, BF_H, timestep);
+    //Met_algo(N, dim, M, a, steplength, HO, alpha, len_alpha, beta, h, num_or_an, BF_H, timestep, one_body);
+    GradientDecent(N, dim, M, a, steplength, HO, beta, h, num_or_an, BF_H, timestep);
 
     return 0;
 }
--- Project 1/Python/plot_ob_density.py
+++ Project 1/Python/plot_ob_density.py
@@ -6,23 +6,23 @@ import os
 # Numerical values
 dirpath = os.getcwd()
 data = np.loadtxt("../data/ob_density.dat")
-data1 = np.loadtxt("../data/ob_density_a_0.dat")
+#data1 = np.loadtxt("../data/ob_density_a_0.dat")
 label_size = {"size":"14"}
 
 r = np.linspace(0, 5, len(data))
 
-N = 10     # Number of particles
+N = 100     # Number of particles
 
 def f(x):
-    return (N/(8*np.pi**(3/2)))*np.exp(-(x**2))
+    return (1.8*N/(8*np.pi**(3/2)))*np.exp(-(x**2))
 
 sns.set()
-plt.plot(r, data1, linewidth=2.0, label="a=0.0 Numerical")
+#plt.plot(r, data1, linewidth=2.0, label="a=0.0 Numerical")
 plt.plot(r, f(r), '--r', linewidth=2.0, label="a=0.0 Analytical")
 plt.plot(r, data, linewidth=2.0, label="a=0.0043")
 #plt.title("One Body Density", **label_size)
 plt.xlabel("r", **label_size)
 plt.ylabel(r"$\rho$", **label_size)
 plt.legend(loc="best")
-plt.savefig("%s/images/ob.png" % dirpath)
+#plt.savefig("%s/images/ob.png" % dirpath)
 plt.show()
