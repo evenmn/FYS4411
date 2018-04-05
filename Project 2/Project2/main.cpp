01a383c090a9084099998cf83e918927e30d5b14
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2018-04-05 16:32:34 +0200

    Eigentest

--- Project 2/Project2/main.cpp
+++ Project 2/Project2/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include "eigen3/Eigen/Dense"
 using namespace std;
 
 int main()
 {
+
+    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(5,5);
+    cout << A << endl;
     cout << "Hello World!" << endl;
     return 0;
 }
