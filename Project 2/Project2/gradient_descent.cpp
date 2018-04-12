89dc86f4bb028a500e0ce68d428d3f98a9aff8d4
evenmn <evenmn@fys.uio.no>
2018-04-12 11:37:52 +0200

    Implemented gradient descent

--- Project 2/Project2/gradient_descent.cpp
+++ Project 2/Project2/gradient_descent.cpp
@@ -25,12 +25,13 @@ void GradientDescent(int P, int D, int N, int MC, double sigma, double omega, do
     int M = P*D;
 
     int M_rand;
+    double eta = 0.001;
 
     //Marsenne Twister Random Number Generator
     normal_distribution<double> eps_gauss(0,1);       //Gaussian distr random number generator
     uniform_int_distribution<> mrand(0, M-1);         //Random number between 0 and N
 
-    for(int iter=0; iter<1; iter++) {
+    for(int iter=0; iter<1000; iter++) {
         //averages and energies
         double EL_tot      = 0;          //sum of energies of all states
         double EL_tot_sqrd = 0;          //sum of energies of all states squared
@@ -47,8 +48,8 @@ void GradientDescent(int P, int D, int N, int MC, double sigma, double omega, do
         VectorXd daE_tot          = VectorXd::Zero(M);
         VectorXd db_tot           = VectorXd::Zero(N);
         VectorXd dbE_tot          = VectorXd::Zero(N);
-        MatrixXd dw_tot           = MatrixXd::Zero(M,N);
-        MatrixXd dwE_tot          = MatrixXd::Zero(M,N);
+        MatrixXd dW_tot           = MatrixXd::Zero(M,N);
+        MatrixXd dWE_tot          = MatrixXd::Zero(M,N);
 
         //cout << W << endl;
 
@@ -89,6 +90,12 @@ void GradientDescent(int P, int D, int N, int MC, double sigma, double omega, do
             Psi.Gradient_b(b, X, W, db);
             Psi.Gradient_W(X, b, W, dW);
 
+            da_tot += da;
+            daE_tot += E*da;
+            db_tot += db;
+            dbE_tot += E*db;
+            dW_tot += dW;
+            dWE_tot += E*dW;
 
             EL_tot += E;
             EL_tot_sqrd += E*E;
@@ -98,6 +105,14 @@ void GradientDescent(int P, int D, int N, int MC, double sigma, double omega, do
         //Calculate <EL> and <EL^2>
         double EL_avg = EL_tot/(MC + 1);
         double EL_avg_sqrd = EL_tot_sqrd/(MC + 1);
+
+        //Gradient descent
+        a -= 2*eta*(daE_tot - EL_avg*da_tot)/MC;
+        b -= 2*eta*(dbE_tot - EL_avg*db_tot)/MC;
+        W -= 2*eta*(dWE_tot - EL_avg*dW_tot)/MC;
+        cout << a << '\n' << endl;
+
+        cout << EL_avg << endl;
 
     }
 }
