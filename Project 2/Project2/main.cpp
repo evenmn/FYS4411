edd591f4946e6432b2372498db705c91f30952cc
evenmn <evenmn@fys.uio.no>
2018-04-26 09:21:47 +0200

    Edited gibbs sampling, does still not work

--- Project 2/Project2/main.cpp
+++ Project 2/Project2/main.cpp
@@ -14,8 +14,8 @@ int main()
     int     N           = 2;              //Number of hidden nodes
     int     MC          = 1000000;        //Number of Monte Carlo cycles
     int     iterations  = 100;             //Number of gradient decent cycles
-    int     sampling    = 1;               //Brute force- (0), Hastings- (1) or Gibbs' sampling (2)
-    bool    interaction = 1;               //Interaction on if true
+    int     sampling    = 0;               //Brute force- (0), Hastings- (1) or Gibbs' sampling (2)
+    bool    interaction = 0;               //Interaction on if true
     bool    one_body    = 0;               //Calculating onebody density if true
     double  sigma       = 1.0;             //Width of Gaussian distribution
     double  omega       = 1.0;             //Frequency
